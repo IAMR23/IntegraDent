c13077cf0455d12b2f8fa7702266438fb9f177f7
IAMRS23 <iasimbanap@gmail.com>
2025-04-03 10:15:46 -0500

    Icons insta

--- src/constsRutas.ts
+++ src/constsRutas.ts
@@ -4,6 +4,7 @@ export const services = [
       img: "./esteticaDental.webp",
       title: "Estética Dental",
       link: "/estetica-dental",
+      icono:  "/icoSonrisa.webp"
     },
     {
       img: "./cirugiaDental.webp",
@@ -19,26 +20,33 @@ export const services = [
       img: "./rehabilitacion.webp",
       title: "Rehabilitación Oral",
       link: "/rehabilitacion",
+      icono : "/icoOrtodoncia.webp"
     },
     { img: "./bruxismo.webp", title: "Bruxismo", link : "/bruxismo" },
     { img: "./carillas.webp", title: "Carillas", link : "/carillas" },
-    { img: "./diseñoSonrisa.webp", title: "Diseño de Sonrisa", link: "/sonrisa" },
+    { img: "./diseñoSonrisa.webp", title: "Diseño de Sonrisa", link: "/sonrisa" 
+       , icono : "/icoSonrisa.webp"
+    },
     {
       img: "./extraccionTerceros.webp",
       title: "Extracción de Muelas del Juicio",
       link: "/extraccion",
     },
-    { img: "./implante1.webp", title: "Implantes Dentales", link: "/implantes" },
+    { img: "./implante1.webp", title: "Implantes Dentales", link: "/implantes" , 
+      icono : "/icoImplantes.webp" },
     {
       img: "./Odontepediatria.webp",
       title: "Odontepediatría",
       link : "/odontepediatria",
+      icono : "/icoOdontopediatria.webp"
     },
     { img: "./odontologia.webp", title: "Odontología", link : "/odontologia" },
     { img: "./allonfour.webp", title: "All on Four", link : "/allonfour" },
     { img: "./protesisFija.webp", title: "Protesis Fija", link : "/protesis" },
     /* Nuevos 21-03-2025 */
-    { img: "./Ortodoncia.webp", title: "Ortodoncia-RO", link : "/Ortodoncia" },
+    { img: "./Ortodoncia.webp", title: "Ortodoncia-RO", link : "/Ortodoncia"  , 
+      icono : "/icoOrtodoncia.webp"
+    },
     /* Cambios 02-04-2025 */
     { img: "./Ortodoncia.webp", title: "Servicio de Docencia", link : "/servicio-docencia" },
   ];
