b006f40b2ccd6e909ac0b7b5bb490e69c6f48dfb
IAMRS23 <iasimbanap@gmail.com>
2025-04-11 10:27:35 -0500

    luck

---
+++ src/const/Cases/case1.ts
@@ -0,0 +1,15 @@
+export const caso1 = [
+  {
+    img: "/ed.webp",
+  },
+  {
+    img: "/ed1.webp",
+  },
+  {
+    img: "/ed2.webp",
+  },
+  {
+    img: "/ed3.webp",
+  },
+  { img: "/ed4.webp" },
+];
